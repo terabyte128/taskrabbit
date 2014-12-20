cfebb580a4a83fa2334328e559f53f2884e7f388
Sam Wolfson <terabyte128@gmail.com>
2014-12-19 17:51:08 -0800

    fixed local migrations

---
+++ taskrabbit/migrations/0007_auto_20141219_1732.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import datetime
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('taskrabbit', '0006_auto_20141219_1412'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='note',
+            name='timestamp',
+            field=models.DateTimeField(default=datetime.datetime(2014, 12, 19, 17, 32, 52, 75384)),
+            preserve_default=True,
+        ),
+    ]
